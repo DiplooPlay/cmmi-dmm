8c948bcf534a90e9e72c3719e9de247311caf543
Diploo <diplooplay@gmail.com>
2026-09-22 17:00:42 -0500

    fix: inline styles and set assets directory to fix styles on Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cmmi-dmm.pages.dev',
+  build: {
+    assets: 'assets',
+    inlineStylesheets: 'always'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
